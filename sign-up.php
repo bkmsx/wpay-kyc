e4ae54a608a821729d80874d0c789ccc60bd8b60
Bkmsx <tien@novum.capital>
2018-06-12 15:43:25 +0700

    add some validations to the site

--- sign-up.php
+++ sign-up.php
@@ -3,20 +3,47 @@ if (isset($_POST['submit'])) {
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 	$confirmPass = $_POST['confirm_pass'];
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		$err = "* Invalid email format";
+		include 'sign-up.html';
+		exit;
+	}
+
+	if (strlen($password) < '6') {
+		$err = "* Password must contain at least 6 characters";
+		include 'sign-up.html';
+		exit;
+    }
+    elseif(!preg_match("#[0-9]+#",$password)) {
+		$err = "* Password must contain number!";
+		include 'sign-up.html';
+		exit;
+    }
+    elseif(!preg_match("#[A-Z]+#",$password)) {
+		$err = "* Password must contain capital letter";
+		include 'sign-up.html';
+		exit;
+    }
+    elseif(!preg_match("#[a-z]+#",$password)) {
+		$err = "* Password must contain lowercase letter";
+		include 'sign-up.html';
+		exit;
+    } elseif($password != $confirmPass) {
+		$err = "* Passwords are not matched";
+		include 'sign-up.html';
+		echo "<script type='text/javascript'>document.getElementsByName('email')[0].value='".$email."';</script>";
+		exit;
+	}
+	
 	require_once('mysqli_connect.php');
 	$query = "select * from bbn_user where email = '$email'";
 	$result = @mysqli_query($dbc, $query);
 	if (mysqli_num_rows($result) > 0){
+		$err = "* Email already exists";
 		include 'sign-up.html';
-			echo "<script type='text/javascript'>document.getElementById('error2').style.display='block';</script>";
-			exit;
+		exit;
 	} else {
-		if($password != $confirmPass) {
-			include 'sign-up.html';
-			echo "<script type='text/javascript'>document.getElementById('error1').style.display='block';";
-			echo "document.getElementsByName('email')[0].value='".$email."';</script>";
-			exit;
-		}
+		
 		$encrypt_pass = password_hash($password, PASSWORD_BCRYPT);
 		$sql_count_row = "select count(user_id) as row_count from bbn_user";
 		$row_count = mysqli_fetch_array(mysqli_query($dbc, $sql_count_row));
